ab0d9cb61a2afec6bdd302d893c602178278cc50
Akshaya <37094521+akshayavenkatesan@users.noreply.github.com>
2019-04-20 23:20:04 +0530

    Add files via upload

---
+++ dynamic_programming/friendspair.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int i,n;
+	cin>>n;
+	int dp[n];
+	dp[1]=1;
+	dp[2]=2;
+	
+	for(i=3;i<=n;i++)
+	{
+		dp[i]=dp[i-1]+(i-1)*dp[i-2];
+	}
+	cout<<dp[n];
+	return 0;
+}
